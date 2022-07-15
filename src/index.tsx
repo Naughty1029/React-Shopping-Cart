a684a9017b5e9bb5de2ae1d959c558e8d7b2c971
watanabe_masahiro02 <watanabe_masahiro@5star.co.jp>
2022-07-15 11:05:38 +0900

    index.tsから不要なもの削除

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import * as ReactDOMClient from 'react-dom/client'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
 
 /* Theme */
 import { ThemeProvider } from 'commons/style/styled-components'
 import { theme } from 'commons/style/theme'
 import GlobalStyle from 'commons/style/global-style'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-  <React.StrictMode>
+const root = document.getElementById('root')!
+const container = ReactDOMClient.createRoot(root)
+
+container.render(
+  <StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <App />
     </ThemeProvider>
-  </React.StrictMode>
+  </StrictMode>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
