25f4ec04991ea20ba590597d460c5429dda6bfec
渡邉雅広 <watanabemasahiro@masahiro.local>
2022-07-16 16:18:56 +0900

    App.tsxを追加

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,36 @@
+import { useEffect } from 'react'
+
+import Products from 'components/Products'
+import Loader from 'components/Loader'
+import Filter from 'components/Filter'
+
+import { useProducts } from 'contexts/products-context'
+
 import * as S from './style'
 
 function App() {
-  return <S.Container>hello</S.Container>
+  const { isFetching, products, fetchProducts } = useProducts()
+
+  useEffect(() => {
+    fetchProducts()
+  }, [fetchProducts])
+
+  return (
+    <S.Container>
+      {isFetching && <Loader />}
+      <S.TwoColumnGrid>
+        <S.Side>
+          <Filter />
+        </S.Side>
+        <S.Main>
+          <S.MainHeader>
+            <p>{products.length} Products found</p>
+          </S.MainHeader>
+          <Products />
+        </S.Main>
+      </S.TwoColumnGrid>
+    </S.Container>
+  )
 }
 
 export default App
--- src/services/products.ts
+++ src/services/products.ts
@@ -3,7 +3,7 @@ import { IGetProductsResponse } from 'models'
 
 export const getProducts = async () => {
   let response: IGetProductsResponse = await axios.get(
-    'https://gist.githubusercontent.com/Naughty1029/f465d40953a349aaf30d4b7f387239c4/raw/675eb98ddcd0be4a7a32b19c5d49fcb25f7a959b/shopping-cart-products'
+    'https://gist.githubusercontent.com/Naughty1029/f465d40953a349aaf30d4b7f387239c4/raw/72efd02501442aa3da2469f3d1330e2231ca04c2/shopping-cart-products'
   )
 
   const { products } = response.data || []
