29f79a9713a2a0493b3f023dea26b5641ee97586
渡邉雅広 <watanabemasahiro@masahiro.local>
2022-07-18 14:24:03 +0900

    Checkbox追加

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,5 +1,39 @@
+import { useProducts } from 'contexts/products-context'
+
+import * as S from './style'
+
+export const availableSizes = ['XS', 'S', 'M', 'ML', 'L', 'XL', 'XXL']
+
 const Filter = () => {
-  return <div>Filter</div>
+  const { filters, filterProducts } = useProducts()
+
+  const selectedCheckboxes = new Set(filters)
+
+  const toggleCheckbox = (label: string) => {
+    if (selectedCheckboxes.has(label)) {
+      selectedCheckboxes.delete(label)
+    } else {
+      selectedCheckboxes.add(label)
+    }
+
+    const filters = Array.from(selectedCheckboxes) as []
+
+    filterProducts(filters)
+  }
+
+  const createCheckbox = (label: string) => (
+    <S.Checkbox label={label} handleOnChange={toggleCheckbox} key={label} />
+  )
+
+  const createCheckboxes = () =>
+    availableSizes.map((label) => createCheckbox(label))
+
+  return (
+    <S.Container>
+      <S.Title>Sizes:</S.Title>
+      {createCheckboxes()}
+    </S.Container>
+  )
 }
 
 export default Filter
